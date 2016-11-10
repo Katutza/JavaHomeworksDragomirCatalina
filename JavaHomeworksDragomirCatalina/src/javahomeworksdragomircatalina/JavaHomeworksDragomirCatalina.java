7af16cd629bf73daa161ede6d1990058a9282022
Katutza <cata.boo@gmail.com>
2016-11-10 20:10:12 +0200

    Merge origin/master

--- JavaHomeworksDragomirCatalina/src/javahomeworksdragomircatalina/JavaHomeworksDragomirCatalina.java
+++ JavaHomeworksDragomirCatalina/src/javahomeworksdragomircatalina/JavaHomeworksDragomirCatalina.java
@@ -1,21 +1,64 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This project is made for my Home Assignments in my Java Course.
  */
 package javahomeworksdragomircatalina;
 
 /**
  *
- * @author gheorgheaurelpacurar
+ * @author Katutza
  */
+import java.util.Scanner;
+        
 public class JavaHomeworksDragomirCatalina {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Scanner input = new Scanner(System.in);// Creating a new Scanner named input
+        System.out.print("Enter your name: ");// Prompting the user for the name
+        String name = input.nextLine();// The crated Scanner reads the line of text from the user
+        System.out.println("Your name is: " + name);// Outputing the input from the user
+        
+        
+   int cat = Integer.parseInt("12", 10); // declaring the int by parsing the string argument
+   System.out.println(cat);
+   
+   int mouse = Integer.parseInt("1010110", 2);// declaring the int by parsing the string argument
+   System.out.println(mouse);
+   
+   int dog = Integer.parseInt("54", 10);// declaring the int by parsing the string argument
+   System.out.println(dog);
+   
+   int parot = Integer.parseInt ("1101100", 2);// declaring the int by parsing the string argument
+   System.out.println(parot);
+   
+   int pets = parot / cat + dog * mouse; // using the arithmetic operators
+   System.out.println("Value after calculating = " + pets);
+   
+   int result = pets % 4; // declaring a new int that will be used later
+   System.out.println(result);
+   
+   boolean condition = true;
+   result = condition ? 1 : 3;
+   
+   result++; 
+   System.out.println(-result); // result is - 2
+   System.out.println(result++); // result is 2; value returned first then incremented
+   System.out.println(++result); // result is 4
+   
+   boolean success = true;
+   if((pets == 4653) && (cat > 11)){ // both conditions must be met
+   System.out.println(success);} 
+   else{
+   System.out.println(!success);}
+   
+   if((pets < 4653) || (cat != 11)){ // one of the conditions must be met
+   System.out.println(success);} 
+   else{
+   System.out.println(!success);}
+   
+   JavaHomeworksDragomirCatalina instance = new JavaHomeworksDragomirCatalina(); //declaring a new obj in the public class 
+   System.out.println(instance instanceof JavaHomeworksDragomirCatalina); // true if the obj declared is an instance of the class
+   }
 }
